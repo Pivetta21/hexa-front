d42742d8ac714a28699f7e4ffb3858cada1b2876
Lucas Pivetta <lucaspivetta21@gmail.com>
2021-06-16 22:54:47 -0300

    feat(navbar): create navbar

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -2,7 +2,21 @@ import { useContext } from 'react';
 import { ThemeContext } from 'styled-components';
 import { Link } from 'react-router-dom';
 
-import { NavContainer } from './styles';
+import Hexa from 'src/assets/svg/logos/Hexa.svg';
+import { ReactComponent as Search } from 'src/assets/svg/icons/Search.svg';
+import { ReactComponent as Moon } from 'src/assets/svg/icons/Moon.svg';
+import { ReactComponent as Sun } from 'src/assets/svg/icons/Sun.svg';
+import { ReactComponent as Profile } from 'src/assets/svg/icons/Profile.svg';
+
+import {
+  NavContainer,
+  NavLogo,
+  NavSearchForm,
+  NavInputSearch,
+  NavInputSeparetor,
+  NavInputButton,
+  NavIcons,
+} from './styles';
 
 interface Props {
   toggleTheme(): void;
@@ -13,26 +27,27 @@ const Navbar: React.FC<Props> = ({ toggleTheme }) => {
 
   return (
     <NavContainer>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/login">Login</Link>
-        </li>
-      </ul>
-      <button
-        style={{
-          borderRadius: '30px',
-          padding: '12px 16px',
-          color: colors.primary,
-        }}
-        onClick={() => {
-          toggleTheme();
-        }}
-      >
-        Theme: {title.charAt(0).toUpperCase() + title.substr(1)}
-      </button>
+      <NavLogo>
+        <Link to="/">
+          <img src={Hexa} alt="Hexa Logo" />
+        </Link>
+      </NavLogo>
+
+      <NavSearchForm>
+        <NavInputSearch type="text" placeholder="Pesquisar..." />
+        <NavInputSeparetor />
+        <NavInputButton type="button">
+          <Search />
+        </NavInputButton>
+      </NavSearchForm>
+
+      <NavIcons>
+        <span onClick={() => toggleTheme()}>
+          {title === 'light' && <Moon fill={colors.icon} />}
+          {title === 'dark' && <Sun fill={colors.icon} />}
+        </span>
+        <Profile fill={colors.icon} />
+      </NavIcons>
     </NavContainer>
   );
 };
